71d5f028acfc479778cb688715c4251cbc6dcbad
young891221 <young891221@gmail.com>
2018-01-28 11:00:53 +0900

    기능 : 질문리스트 불러오기

--- src/main/java/codesquad/web/HomeController.java
+++ src/main/java/codesquad/web/HomeController.java
@@ -1,14 +1,25 @@
 package codesquad.web;
 
+import codesquad.service.QnaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import static org.springframework.data.domain.Sort.Direction.DESC;
+
 @Controller
 public class HomeController {
 
+    @Autowired
+    private QnaService qnaService;
+
     @GetMapping("/")
-    public String home(Model model) {
+    public String home(@PageableDefault(sort = "id", direction = DESC) Pageable pageable,
+                       Model model) {
+        model.addAttribute("questions", qnaService.findAll(pageable));
         return "home";
     }
 }
